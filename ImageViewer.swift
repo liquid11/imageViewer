3647a23712df136b0717bb5d834ce58ba253e81a
liquid11 <orangevolution@yahoo.com>
2015-05-15 21:13:14 +0500

    view controller class addition

---
+++ ImageViewer.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoViewer.swift
+//  imageViewer
+//
+//  Created by Tezro Solutions on 15/05/2015.
+//  Copyright (c) 2015 Tezro Solutions. All rights reserved.
+//
+
+import Foundation
+
+struct Photo {
+
+    var name: String
+    var filename :String
+    var notes: String
+}
